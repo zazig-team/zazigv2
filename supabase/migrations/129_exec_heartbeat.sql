68a29c933042309ca9c8d6d591fb520f82d716c7
Chris Evans <chris.evans1@gmail.com>
2026-03-10 16:24:19 +1100

    129 migration fixed

--- supabase/migrations/129_exec_heartbeat.sql
+++ supabase/migrations/129_exec_heartbeat.sql
@@ -1,6 +1,6 @@
-ALTER TABLE roles ADD COLUMN cache_ttl_minutes INTEGER;
-ALTER TABLE roles ADD COLUMN hard_ttl_minutes INTEGER DEFAULT 240;
-ALTER TABLE roles ADD COLUMN heartbeat_md TEXT;
+ALTER TABLE roles ADD COLUMN IF NOT EXISTS cache_ttl_minutes INTEGER;
+ALTER TABLE roles ADD COLUMN IF NOT EXISTS hard_ttl_minutes INTEGER DEFAULT 240;
+ALTER TABLE roles ADD COLUMN IF NOT EXISTS heartbeat_md TEXT;
 
 UPDATE roles
 SET cache_ttl_minutes = 30, heartbeat_md = ''

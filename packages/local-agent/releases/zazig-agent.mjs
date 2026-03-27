a0647799a4155b995eca490b0099e99f7e3f89ce
Chris Evans <chris.evans1@gmail.com>
2026-03-27 13:57:39 +1100

    chore: update production bundles and bump version to 0.49.0

--- packages/local-agent/releases/zazig-agent.mjs
+++ packages/local-agent/releases/zazig-agent.mjs
@@ -1,4 +1,4 @@
-const AGENT_BUILD_HASH = "b139357";
+const AGENT_BUILD_HASH = "6b008b1";
 import { createRequire } from "module"; const require = createRequire(import.meta.url);
 var __create = Object.create;
 var __defProp = Object.defineProperty;

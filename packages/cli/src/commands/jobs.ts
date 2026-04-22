950cdedfc3527537e77128487560e0b569b3e5c2
Chris Evans <chris.evans1@gmail.com>
2026-04-22 10:57:47 +1000

    fix(cli): relax jobs UUID validation format

--- packages/cli/src/commands/jobs.ts
+++ packages/cli/src/commands/jobs.ts
@@ -8,7 +8,7 @@ type ParsedFlag = {
 };
 
 const UUID_V4ISH_REGEX =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 function parseFlag(args: string[], name: string): ParsedFlag {
   const eq = args.find((arg) => arg.startsWith(`--${name}=`));
